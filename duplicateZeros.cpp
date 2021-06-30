1679c5aebfd85314aaaf58b0940c9ea01d56988b
mfaisalshahid <56493157+mfaisalshahid@users.noreply.github.com>
2021-06-30 05:03:07 -0400

    Create duplicateZeros.cpp
    Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
    
    Note that elements beyond the length of the original array are not written.
    
    Do the above modifications to the input array in place, do not return anything from your function.

---
+++ duplicateZeros.cpp
@@ -0,0 +1,10 @@
+void duplicateZeros(vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == 0) {
+            for (int j = arr.size() - 1; j > i; j--) {
+                arr[j] = arr[j-1];
+            }
+            i++; // we don't want to traverse over the duplicate zero
+        }
+    }
+}
