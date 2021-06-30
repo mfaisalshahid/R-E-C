320b6c15b68b7db21320b15d98aa943930fb6ca0
mfaisalshahid <56493157+mfaisalshahid@users.noreply.github.com>
2021-06-30 05:04:03 -0400

    Create addStrings.cpp
    Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
    
    You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.

---
+++ addStrings.cpp
@@ -0,0 +1,12 @@
+string addStrings(string num1, string num2) {
+  int i=num1.size()-1,j=num2.size()-1,carry=0;
+  string res="";
+  while(i>=0||j>=0)
+  {
+      if(i>=0) carry+=num1[i--]-'0';
+      if(j>=0) carry+=num2[j--]-'0';
+      res=to_string(carry%10) + res;
+      carry/=10;
+  }
+  return carry?"1"+res:res;
+ }
